67d0bc52946dadbde0c3ca63b1a237a15d6d3511
abhinash1998 <abhinashb@mindfiresolutions.com>
2023-02-17 23:44:04 +0530

    Refactoring the code of Navbar Component

---
+++ airport-fuel-inventory/src/NavbarMenu/navbar-menu.js
@@ -0,0 +1,22 @@
+export const navbarMenuItems = [
+	{
+		menuOption: 'Airport',
+		menuLink: 'airport'
+	},
+	{
+		menuOption: 'Aircraft',
+		menuLink: 'aircraft'
+	},
+	{
+		menuOption: 'Transaction',
+		menuLink: 'transaction'
+	},
+	{
+		menuOption: 'Airport Consumption Report',
+		menuLink: 'airport-consumption-report'
+	},
+	{
+		menuOption: 'Fuel Consumption Report',
+		menuLink: 'fuel-consumption-report'
+	}
+];
--- airport-fuel-inventory/src/components/Navbar.vue
+++ airport-fuel-inventory/src/components/Navbar.vue
@@ -1,74 +1,50 @@
 <template>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-color">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="#">Airport Fuel Inventory</a>
-			<button
-				class="navbar-toggler"
-				type="button"
-				data-bs-toggle="collapse"
-				data-bs-target="#navbarText"
-				aria-controls="navbarText"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-			>
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarText">
+			<span class="navbar-brand">Airport Fuel Inventory</span>
+			<div class="collapse navbar-collapse">
 				<ul class="navbar-nav mr-auto mb-2 mb-lg-0 navbar-margin">
-					<li>
-						<router-link class="nav-link" to="airport">
-							Airport
-						</router-link>
-					</li>
-					<li>
-						<router-link class="nav-link" to="aircraft">
-							Aircraft
-						</router-link>
-					</li>
-					<li>
-						<router-link class="nav-link" to="transaction">
-							Transaction
-						</router-link>
-					</li>
-					<li>
-						<router-link
-							class="nav-link"
-							to="airportConsumptionReport"
-						>
-							Airport Consumption Report
-						</router-link>
-					</li>
-					<li>
-						<router-link
-							class="nav-link"
-							to="fuelConsumptionReport"
-						>
-							Fuel Consumption Report
-						</router-link>
-					</li>
+					<div
+						v-for="item in navbarMenuItems"
+						v-bind:key="item.menuOption"
+					>
+						<NavbarMenuItem
+							v-bind:menuOption="item.menuOption"
+							v-bind:menuLink="item.menuLink"
+						/>
+					</div>
 				</ul>
 
-				<span class="navbar-text cursor-pointer" v-on:click="logOut()">Logout</span>
+				<span class="navbar-brand cursor-pointer" v-on:click="logOut()">
+					Logout
+				</span>
 			</div>
 		</div>
 	</nav>
 </template>
 
 <script>
+import NavbarMenuItem from './NavbarMenuItem.vue';
+import {navbarMenuItems} from '../NavbarMenu/navbar-menu'
+
 export default {
 	name: 'Navbar',
+	components: {
+		NavbarMenuItem
+	},
 	methods: {
-		isLoggedIn: function () {
-			if (this.$cognitoAuth.getCurrentUser() === null) {
-				return false;
-			} else {
-				return true;
-			}
-		},
 		logOut() {
-			localStorage.setItem('token',null);
+			localStorage.setItem('token', null);
 			this.$router.push('/');
 		}
+	},
+	data() {
+		return {
+			navbarMenuItems: []
+		};
+	},
+	mounted() {
+		this.navbarMenuItems = navbarMenuItems;
 	}
 };
 </script>
